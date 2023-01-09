4e8f97fd79cd754c72f891d8dbb4c1680ac8b41c
Vladislav Ponomarev <Git@PonomareVlad.ru>
2023-01-09 16:05:18 +0500

    Fixed images public path

--- src/config.mjs
+++ src/config.mjs
@@ -6,8 +6,7 @@ if (MAX_EVENTS) config.limit = parseInt(MAX_EVENTS);
 if (API_KEYS && config.api) config.api.keys = JSON.parse(API_KEYS);
 if (VERCEL_URL && !config.images?.url) {
     if (!config.images) config.images = {};
-    const url = new URL(output, `https://${VERCEL_URL}`);
-    config.images.url = new URL(config.images.path || './', url).href;
+    config.images.url = new URL(config.images.path || './', `https://${VERCEL_URL}`).href;
 }
 config.output = new URL(output, new URL('../', import.meta.url));
 export default config;
